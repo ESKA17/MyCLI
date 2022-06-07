89edf01b6e58a9d9126a272bb6222f6cdda78f8d
sabit.kurmashev <sabakurmashev@gmail.com>
2022-06-07 16:40:15 +0600

    Polishing 2 Solid 4

--- src/main/java/com/example/mycli/controller/AccountTransactionController.java
+++ src/main/java/com/example/mycli/controller/AccountTransactionController.java
@@ -45,90 +45,73 @@ public class AccountTransactionController {
 
     @GetMapping("/{account_id}")
     ResponseEntity<?> getAccount(@PathVariable String account_id) {
-        ResponseEntity<?> match = mathcer(account_id);
-        if (Objects.equals(match.getStatusCode(), HttpStatus.OK)) {
-                Account out = accountRepositoryDAO.findById(account_id).orElseThrow(() ->
-                        new AccountNotFound(account_id));
-                return ResponseEntity.status(HttpStatus.OK).body(out);
-        } else {
-            return match;
+        if (!account_id.matches("[0-9]+")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong input!");
         }
+        Account out = accountRepositoryDAO.findById(account_id).orElseThrow(() ->
+                new AccountNotFound(account_id));
+        return ResponseEntity.status(HttpStatus.OK).body(out);
     }
 
     @DeleteMapping("/{account_id}")
     ResponseEntity<?> deleteAccount(@PathVariable String account_id) {
-        ResponseEntity<?> match = mathcer(account_id);
-        if (Objects.equals(match.getStatusCode(), HttpStatus.OK)) {
-            accountRepositoryDAO.deleteById(account_id);
-            return ResponseEntity.status(HttpStatus.OK).body("Account was deleted");
-        } else {
-            return match;
+        if (!account_id.matches("[0-9]+")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong input!");
         }
+        if (accountRepositoryDAO.findById(account_id).isEmpty()) throw new AccountNotFound(account_id);
+        accountRepositoryDAO.deleteById(account_id);
+        return ResponseEntity.status(HttpStatus.OK).body("Account was deleted");
     }
 
     @PostMapping("/{account_id}/withdraw")
     ResponseEntity<?> withdrawMoney(@PathVariable String account_id, @RequestParam double amount) {
-        ResponseEntity<?> match = mathcer(account_id);
-        if (Objects.equals(match.getStatusCode(), HttpStatus.OK)) {
-            Account account = accountRepositoryDAO.findById(account_id).orElseThrow(() ->
-                    new AccountNotFound(account_id));
-            if (!account.isWithdrawalAllowed()) {
-                return ResponseEntity.status(HttpStatus.OK).body("Withdrawal is not allowed!");
-            }
-            if (amount > 0) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount should be greater than 0!");
-            }
-            if (account.getBalance() >= amount) {
-                transactionWithdraw.execute((AccountWithdraw) account, amount);
-                return ResponseEntity.status(HttpStatus.OK).body(String.format("%.2f", amount) +
-                        "$ transferred to " + account_id + " account");
-            } else {
-                return ResponseEntity.status(HttpStatus.OK).body("Not enough funds!");
-            }
-        } else {
-            return match;
+        if (!account_id.matches("[0-9]+")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong input!");
         }
-
+        Account account = accountRepositoryDAO.findById(account_id).orElseThrow(() ->
+                new AccountNotFound(account_id));
+        if (!account.isWithdrawalAllowed()) {
+            return ResponseEntity.status(HttpStatus.OK).body("Withdrawal is not allowed!");
+        }
+        if (amount > 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount should be greater than 0!");
+        }
+        if (account.getBalance() >= amount) {
+            transactionWithdraw.execute((AccountWithdraw) account, amount);
+            return ResponseEntity.status(HttpStatus.OK).body(String.format("%.2f", amount) +
+                    "$ transferred to " + account_id + " account");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body("Not enough funds!");
+        }
     }
 
     @PostMapping("/{account_id}/deposit")
     ResponseEntity<?> depositMoney(@PathVariable String account_id, @RequestParam double amount) {
-        ResponseEntity<?> match = mathcer(account_id);
-        if (Objects.equals(match.getStatusCode(), HttpStatus.OK)) {
-            if (amount > 0) {
-                Account account = accountRepositoryDAO.findById(account_id).orElseThrow(() ->
-                        new AccountNotFound(account_id));
-                transactionDeposit.execute(account, amount);
-                return ResponseEntity.status(HttpStatus.OK).body(String.format("%.2f", amount) +
-                        "$ transferred to " + account_id + " account");
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount should be greater than 0");
-            }
-        } else {
-            return match;
-        }
-
-    }
-    @GetMapping("/{account_id}/transactions")
-    ResponseEntity<?> getAllTransactions(@PathVariable String account_id) {
-        ResponseEntity<?> match = mathcer(account_id);
-        if (Objects.equals(match.getStatusCode(), HttpStatus.OK)) {
-            List<Transaction> out = new ArrayList<>();
-            Iterable<Transaction> iterable = transactionRepositoryDAO.findAll();
-            iterable.forEach(transaction -> {
-                if (Objects.equals(transaction.getId(), account_id)) out.add(transaction);
-            });
-            return ResponseEntity.status(HttpStatus.OK).body(out);
-        } else {
-            return match;
-        }
-
-    }
-    ResponseEntity<?> mathcer(String account_id) {
         if (!account_id.matches("[0-9]+")) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong input!");
         }
-        accountRepositoryDAO.findById(account_id).orElseThrow(() -> new AccountNotFound(account_id));
-        return ResponseEntity.status(HttpStatus.OK).body("");
+        if (amount > 0) {
+            Account account = accountRepositoryDAO.findById(account_id).orElseThrow(() ->
+                    new AccountNotFound(account_id));
+            transactionDeposit.execute(account, amount);
+            return ResponseEntity.status(HttpStatus.OK).body(String.format("%.2f", amount) +
+                    "$ transferred to " + account_id + " account");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount should be greater than 0");
+        }
     }
+    @GetMapping("/{account_id}/transactions")
+    ResponseEntity<?> getAllTransactions(@PathVariable String account_id) {
+        if (!account_id.matches("[0-9]+")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong input!");
+        }
+        if (accountRepositoryDAO.findById(account_id).isEmpty()) throw new AccountNotFound(account_id);
+        List<Transaction> out = new ArrayList<>();
+        Iterable<Transaction> iterable = transactionRepositoryDAO.findAll();
+        iterable.forEach(transaction -> {
+            if (Objects.equals(transaction.getId(), account_id)) out.add(transaction);
+        });
+        return ResponseEntity.status(HttpStatus.OK).body(out);
+    }
+
 }
